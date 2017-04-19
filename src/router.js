a1ae64fe05b6ad377e648de712ec389d9291a236
bobo2007 <824060993@qq.com>
2017-04-19 13:23:17 +0800

    完善获取对应页面组件与请求数据功能

--- src/router.js
+++ src/router.js
@@ -2,11 +2,11 @@
  * File Name: workplace/proj/src/router.js
  * Created By: bobo2007
  * Creation Date: 2017-04-12 13:24:11
- * Last Modified: 2017-04-18 20:08:44
+ * Last Modified: 2017-04-19 13:20:46
  * Purpose: 解析路径并获取路径对应的页面组件及其数据
  */
-
-import react from 'react';
+// 注意 React 必须R 是大写,否则eslint报错
+import React from 'react';
 
 function decodeParam(val) {
   if (!(typeof val === 'string' || val.length === 0)) {
@@ -32,6 +32,7 @@ function matchURI(route, path) {
   }
   const params = Object.create(null);
   for (let i = 1; i < match.length; i += 1) {
+    // 收集路径中匹配的各个参数，不匹配的为undefined
     params[route.keys[i - 1].name] = match[i] !== undefined ? decodeParam(match[i]) : undefined;
   }
   return params;
@@ -41,21 +42,53 @@ function matchURI(route, path) {
 function resolve(routes, context) {
   for (const route of routes) {
     const params = matchURI(route, context.error ? '/error' : context.pathname);
+    // 如果不匹配当前路径,继续遍历
     if (!params) {
       continue;
     }
     // 检查route中是否有需要请求的数据
-    // {path: '/task/:id', data: {task: 'GET /api/tasks/:id'}, page: './pages/task'}
+    // {path: '/task/:id', data: {task: 'GET /api/tasks/$id'}, page: './pages/task'}
     if (route.data) {
-      // 同步加载页面组件和请求数据
-      const keys = Object.keys(route.data);
+      // 如果有请求加载数据就同步加载页面组件和请求数据
+      const tasks = Object.keys(route.data);
       return Promise.all([
-        route.road(),
-        ...keys.map((key) => {
-          const query = route.data[key];
-          const method = query.substring(0, query.indexOf(' '));
+        // 加载页面
+        route.load(),
+        // 获取数据
+        ...tasks.map((task) => {
+          const query = route.data[task];
+          // 获取请求方法
+          const method = query.substring(0, query.indexOf(' ')); // GET
+          let url = query.substr(query.indexOf(' ') + 1); // /api/tasks/$id
+          Object.keys(params).forEach((k) => {
+            url = url.replace(`${k}`, params[k]);
+          });
+          // 注意返回的是promise实例
+          return fetch(url, {
+            method
+          }).then((resp) => {
+            resp.json();
+          });
         })
-      ]);
+      ]).then(([Page, ...data]) => {
+        const props = tasks.reduce((result, task, i) => ({
+          ...result,
+          [task]: data[i]
+        }), {});
+        return <Page route={{ ...route,params }} error={context.error} {...props} />;
+      });
     }
+    // 如果没有需要请求的数据就直接加载页面
+    return route.load().then((Page) => {
+      return <Page route={{ ...route,params }} error={context.error}  />;
+    });
   }
+  // 没有找到要访问的页面
+  const error = new Error('Page not found');
+  error.status = 404;
+  return Promise.reject(error);
 }
+
+export default {
+  resolve
+};
