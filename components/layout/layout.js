494192d229f3a4701f6ce1be52027cb166c1aa24
bobo2007 <824060993@qq.com>
2017-04-24 15:43:40 +0800

    完成style部分

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -2,7 +2,7 @@
  * File Name: components/layout/layout.js
  * Created By: bobo2007
  * Creation Date: 2017-04-20 13:29:28
- * Last Modified: 2017-04-20 15:07:30
+ * Last Modified: 2017-04-24 11:49:05
  * Purpose:
  */
 
@@ -26,38 +26,38 @@ class Basic extends React.Component{
     let hasSider;
     if (name === 'Layout') {
       React.Children.forEach(children, (ele) => {
-          if (ele && ele.props && ele.props.name  === 'Sider') {
+        if (ele && ele.props && ele.props.name  === 'Sider') {
           hasSider = true;
-          }
-          });
+        }
+      });
     }
     const divCls = classNames(className, prefixCls, {
-        [`${prefixCls}-has-sider`]: hasSider,
-        });
+      [`${prefixCls}-has-sider`]: hasSider,
+    });
     return (
-        <div className={divCls} {...others}>{childern}</div>
-        );
+      <div className={divCls} {...others}>{childern}</div>
+    );
   }
 }
 
 const Layout = generator({
-prefixCls: 'boge-layout',
-name: 'Layout'
+  prefixCls: 'boge-layout',
+  name: 'Layout'
 })(Basic);
 
 const Header = generator({
-prefixCls: 'boge-layout-header',
-name: 'Header'
+  prefixCls: 'boge-layout-header',
+  name: 'Header'
 })(Basic);
 
 const Footer = generator({
-prefixCls: 'boge-layout-footer',
-name: 'Footer'
+  prefixCls: 'boge-layout-footer',
+  name: 'Footer'
 })(Basic);
 
 const Content = generator({
-prefixCls: 'boge-layout-content',
-name: 'Content'
+  prefixCls: 'boge-layout-content',
+  name: 'Content'
 })(Basic);
 
 Layout.Header = Header;
