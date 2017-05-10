fc2fd4830bb6976ada900cc9288a28d7e1650529
bobo2007 <824060993@qq.com>
2017-05-10 12:13:04 +0800

    sider

--- components/layout/sider.js
+++ components/layout/sider.js
@@ -2,8 +2,8 @@
  * File Name: components/layout/sider.js
  * Created By: bobo2007
  * Creation Date: 2017-04-20 16:01:10
- * Last Modified: 2017-05-05 22:58:37
- * Purpose: 可收起的侧边栏，自带默认样式及基本功能，其下可嵌套任何元素，只能放在 Layout 中.
+ * Last Modified: 2017-05-10 12:12:38
+ * Purpose: 可收起的侧边栏，自带默认样式及基本功能，其下可嵌套任何元素，只能放在 Layout 中
  */
 
 import React from 'react' ;
@@ -57,15 +57,15 @@ export default class Sider extends React.Component{
     }
   }
 
-  setCollapsed = (collapsed) => {
-    if (!('collapsed' in nextProps)) {
+  setCollapsed = (collapsed, type) => {
+    if (!('collapsed' in this.props)) {
       this.setState({
         collapsed
       });
     }
     const { onCollapse } = this.props;
     if (onCollapse) {
-      onCollapse(collapsed);
+      onCollapse(collapsed, type);
     }
   }
 
@@ -80,10 +80,41 @@ export default class Sider extends React.Component{
     } = this.props;
     const divProps = omit(others, ['collapsed','defaultCollapsed','onCollapse','breakpoint']);
     const siderWidth = this.state.collapsed ? collapsedWidth : width;
-    const zeroWidthTrigger = collapseWidth === 0 || collapseWidth  === '0' ? (
-      <span onClick={this.toggle} classname=''>
-
+    const zeroWidthTrigger = collapsedWidth === 0 || collapsedWidth  === '0' ? (
+      <span onClick={this.toggle} classname={`${prefixCls}-zero-width-trigger`}>
+        <Icon type="bars" />
       </span>
+    ) : null;
+
+    const iconObj = {
+      'expand': reverseArrow ? <Icon type="right" /> : <Icon type="left" />,
+      'collapsed': reverseArrow ? <Icon type="left" /> : <Icon type="right" />
+    };
+    const status = this.state.collapsed ? 'collapsed' : 'expanded';
+    const defaultTrigger = iconObj[status];
+
+    const triggerDom = (
+      trigger  !== null ? zeroWidthTrigger || (
+          <div className={`${prefixCls}-trigger`} onClick={this.toggle}>
+            {trigger || defaultTrigger}
+          </div>
+        ) : null);
+    const divStyle = {
+      ...style,
+      flex: `0 0 ${siderWidth}px`,
+      width: `${siderWidth}px`
+    };
+    const siderCls = classNames(className, prefixCls, {
+      [`${prefixCls}-collapsed`] : !!this.state.collapsed,
+      [`${prefixCls}-has-trigger`]: !!trigger,
+      [`${prefixCls}-below`]: !!this.state.below,
+      [`${prefixCls}-zero-width`]: siderWidth  === 0 || siderWidth  === '0'
+    });
+    return (
+      <div className={siderCls} {...divProps} style={divStyle}>
+        {this.props.children}
+        {collapsible || (this.state.below && zeroWidthTrigger) ? triggerDom : null}
+      </div>
     );
   }
 }
