c57c16f23474a6ce4552841e685503f04cd27262
bobo2007 <824060993@qq.com>
2017-05-25 17:28:19 +0800

    test

--- src/history.js
+++ src/history.js
@@ -3,7 +3,7 @@
  * Created By: bobo2007
  * Creation Date: 2017-04-14 00:09:27
  * Last Modified: 2017-04-14 00:09:55
- * Purpose:
+ * Purpose: history
  */
 
 import createHistory from 'history/createBrowserHistory';
